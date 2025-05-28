3482698682b77a9563799f1337329189cdf57cec
kola <koli>
2025-05-28 21:17:31 +0300

    init

--- components/about/page.tsx
+++ components/about/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="px-6 py-10 bg-gray-50 text-center">
       <h2 className="text-2xl font-bold text-gray-800 mb-4">О нас</h2>
       <p className="text-gray-600 max-w-xl mx-auto">
-        Это небольшой блок "О нас", который ты можешь отредактировать. Здесь можно написать, чем ты занимаешься, в чём суть проекта, и почему это важно.
+        Это небольшой блок &quot;О нас&quot;, который ты можешь отредактировать. Здесь можно написать, чем ты занимаешься, в чём суть проекта, и почему это важно.
       </p>
     </section>
   )
